bd0afa0c6ccbf3dcb3f4b67c5493b12e7058be27
Steven Luu <sluu123@gmail.com>
2025-10-24 18:29:58 -0400

    Initial commit of MERN stack project with working Docker Compose configuration

---
+++ client/src/main.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Record from "./components/Record";
+import RecordList from "./components/RecordList.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <RecordList />,
+      },
+    ],
+  },
+  {
+    path: "/edit/:id",
+    element: <App />,
+    children: [
+      {
+        path: "/edit/:id",
+        element: <Record />,
+      },
+    ],
+  },
+  {
+    path: "/create",
+    element: <App />,
+    children: [
+      {
+        path: "/create",
+        element: <Record />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
